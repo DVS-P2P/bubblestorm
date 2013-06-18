f45aedd91f29f5aeeee8e753010a98f63049db44
Max Lehn <mlehn@dvs.tu-darmstadt.de>
2013-06-18 13:43:38 +0200

    build pure CUSP lib for Android, too

--- bindings/java/android/jni/Android.mk
+++ bindings/java/android/jni/Android.mk
@@ -1,10 +1,24 @@
 LOCAL_PATH:= $(call my-dir)
 
+# BubbleStorm (incl. CUSP)
+
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := bsjni
 LOCAL_CFLAGS    := -Wall -DLIBRARY_NAME=bubblestorm -DDISABLE_SIGNALS -I../../c/android-arm -I../../common -I../../cusp -I../../bubblestorm
 LOCAL_SRC_FILES := ../../bubblestorm/bs-jni.c
 LOCAL_LDLIBS    := -L../../c/android-arm -lbubblestorm -llog
+
+include $(BUILD_SHARED_LIBRARY)
+
+
+# CUSP only
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := cuspjni
+LOCAL_CFLAGS    := -Wall -DLIBRARY_NAME=cusp -DDISABLE_SIGNALS -I../../c/android-arm -I../../common -I../../cusp
+LOCAL_SRC_FILES := ../../cusp/cusp-jni.c
+LOCAL_LDLIBS    := -L../../c/android-arm -lcusp -llog
 
 include $(BUILD_SHARED_LIBRARY)
