af30def4aae228b6da7d94b501614e6760097135
Robert Rehner <rehner@dvs.tu-darmstadt.de>
2014-07-10 12:02:02 +0200

    Hack to make work-around windows linking issue for file descriptor hack work

--- bindings/c/common/libmain.c
+++ bindings/c/common/libmain.c
@@ -6,3 +6,5 @@
 
 /// HACK: need a ~1 as Socket.sock_desc, conversion is gone since MLton.Socket has been removed
 extern int32_t fakeInvalidSd() { return -1; }
+
+extern int32_t _imp__fakeInvalidSd() { return -1; }
